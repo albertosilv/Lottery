358a69364115b3c6e68115249aa866ac68fbdef1
albertosilv <jose.alberto.silva@ccc.ufcg.edu.br>
2021-08-04 09:19:28 -0300

    preenchimento do botao do choose game e informaçao do carinho vazio

--- index.js
+++ index.js
@@ -14,7 +14,6 @@
         return {
             init: function init() {
                 this.gamesInfo()
-                this.chooseGame()
             },
             gamesInfo: function gamesInfo() {
                 ajax.open('GET', './games.json', true);
@@ -24,77 +23,92 @@
             getGamesInfo: function getGamesInfo() {
                 if (!app.isReady.call(this)) return;
                 rules = JSON.parse(this.responseText).types;
-                app.generateNumbers('Lotofácil')
+                app.chooseGame()
+                typeGame=rules[0]
+                app.generateNumbers()
             },
             isReady: function isReady() {
                 return this.readyState === 4 && this.status === 200
             },
             chooseGame: function chooseGame() {
-
-                const buttonFacil = doc.getElementById('button-facil')
-                const buttonMega = doc.getElementById('button-mega')
-                const buttonMania = doc.getElementById('button-mania')
+                this.createButtons()
                 const buttonClear = doc.getElementById('clear-game')
                 const buttonComplete = doc.getElementById('complete-game')
                 const buttonAddCar = doc.getElementById('submit')
-                buttonFacil.addEventListener('click', this.handleGame)
-                buttonMega.addEventListener('click', this.handleGame)
-                buttonMania.addEventListener('click', this.handleGame)
                 buttonClear.addEventListener('click', this.clearGame)
                 buttonComplete.addEventListener('click', this.completeGame)
                 buttonAddCar.addEventListener('click', this.addCar)
-
             },
-            handleGame: function handleGame(e) {
-                const idButton = e.srcElement.id
-                gamesNumber['Lotofácil'] = []
-                gamesNumber['Mega-Sena'] = []
-                gamesNumber['Quina'] = []
-                switch (idButton) {
-                    case 'button-facil':
-                        app.generateNumbers('Lotofácil');
-                        break;
-                    case 'button-mega':
-                        app.generateNumbers('Mega-Sena');
-                        break;
-                    case 'button-mania':
-                        app.generateNumbers('Quina');
-                        break;
-                    default:
-                        alert('error')
-                        break;
-                }
+            createButtons: function createButtons() {
+                const containerButton = doc.getElementById('buttonGames')
+                containerButton.innerHTML = ''
+                rules.forEach((element) => {
+                    gamesNumber[element.type] = []
+                    const button = doc.createElement('button')
+                    button.setAttribute('class', 'button-game  col-lg-2 col-md-4 col-4 my-1')
+                    button.style.border = `${element.color} solid 2px`
+                    button.setAttribute('active', 'false')
+                    button.style.color = element.color;
+                    button.style.backgroundColor = '#fff';
+                    button.append(doc.createTextNode(`${element.type}`))
+                    button.addEventListener('click', () => this.handleGame(element, button))
+                    containerButton.appendChild(button)
+                })
             },
-            generateNumbers: function generateNumbers(type) {
-                const game = rules.filter(e => {
-                    return e.type === type
-                })[0]
-                typeGame = game;
+            handleGame: function handleGame(gameActive, element) {
+                typeGame=gameActive
+                const backgroundColor = element.style.backgroundColor
+                const color = element.style.color
+                element.style.color = backgroundColor;
+                element.style.backgroundColor = color;
+                element.setAttribute('active', 'true')
+                const buttons = document.querySelectorAll('.button-game');
+                buttons.forEach(e => {
+                    if (e != element) {
+                        if (e.getAttribute('active') == 'true') {
+                            const backgroundColor = e.style.backgroundColor
+                            const color = e.style.color
+                            e.style.color = backgroundColor;
+                            e.style.backgroundColor = color;
+                            e.setAttribute('active', 'false')
+                        }
+                    }
+                })
+                this.generateNumbers()
+            },
+            generateNumbers: function generateNumbers() {
                 this.descriptionGame()
                 const containerNumbers = doc.getElementById('container-numbers')
                 containerNumbers.innerHTML = ''
-                for (let i = 1; i <= game.range; i++) {
+                for (let i = 1; i <= typeGame.range; i++) {
                     containerNumbers.appendChild(app.generateNumberButton(i < 10 ? `0${i}` : i))
                 }
             },
-            descriptionGame: function descriptionGame(){
+            descriptionGame: function descriptionGame() {
                 const $description = doc.getElementById('description')
-                $description.innerHTML=''
+                $description.innerHTML = ''
                 $description.append(doc.createTextNode(typeGame.description))
-                $description.setAttribute('style','color:#887979dc')
+                $description.setAttribute('style', 'color:#887979dc')
             },
             generateNumberButton: function generateNumberButton(number) {
                 const $button = doc.createElement('button')
                 const $text = doc.createTextNode(number)
                 $button.setAttribute('class', 'button-number')
+                $button.setAttribute('active','false')
                 $button.setAttribute('id', number)
                 $button.addEventListener('click', () => {
                     const length = typeGame['max-number'] - gamesNumber[typeGame.type].length
-                    if (length !== 0) {
-                        gamesNumber[`${typeGame.type}`].push(String(number))
-                        $button.removeAttribute('class', 'button-number')
-                        $button.setAttribute('class', 'button-number-selection')
+                    if($button.getAttribute('active')=='false'){
+                        if (length !== 0) {
+                            gamesNumber[`${typeGame.type}`].push(String(number))
+                            $button.removeAttribute('class', 'button-number')
+                            $button.setAttribute('class', 'button-number-selection')
+                            $button.setAttribute('active','true')
+                        }
+                    }else{
+
                     }
+                    
                 })
                 $button.append($text)
                 return $button
@@ -108,7 +122,11 @@
                 gamesNumber[typeGame.type] = []
             },
             completeGame: function completeGame() {
-                const length = typeGame['max-number'] - gamesNumber[typeGame.type].length
+                let length = typeGame['max-number'] - gamesNumber[typeGame.type].length
+                if(length==0){
+                    gamesNumber[typeGame.type]=[]
+                    length = typeGame['max-number'] - gamesNumber[typeGame.type].length
+                }
                 let cont = 0
                 while (true) {
                     const num = Math.round(Math.random() * 100)
@@ -166,13 +184,13 @@
 
 
                 $type.append(doc.createTextNode(`${typeGame.type}`))
-                $type.style.color=typeGame.color;
+                $type.style.color = typeGame.color;
                 $text.appendChild($type)
                 $text.append(doc.createTextNode(` R$ ${typeGame.price.toFixed(2)}`))
-                $text.setAttribute('style','color:#887979dc')
+                $text.setAttribute('style', 'color:#887979dc')
                 const text = doc.createTextNode(gamesNumber[typeGame.type].sort().toString())
                 $textNumber.append(text)
-                $textNumber.setAttribute('style','color:#887979dc')
+                $textNumber.setAttribute('style', 'color:#887979dc')
 
                 $containerText.appendChild($textNumber)
                 $containerText.appendChild($text)
@@ -183,10 +201,10 @@
                 const cart = doc.getElementById('cart')
                 cart.appendChild($container)
 
-                $button.addEventListener('click',()=>this.removeCartGame($container))
+                $button.addEventListener('click', () => this.removeCartGame($container))
                 contGame++;
             },
-            removeCartGame:function removeCartGame(element){
+            removeCartGame: function removeCartGame(element) {
                 const cart = doc.getElementById('cart')
                 const game = element.id.split('-')[1]
                 cart.removeChild(element);
@@ -203,25 +221,14 @@
                 amountComponente.append(text)
                 this.clearGame()
             },
-            removeAmoutGame:function removeAmoutGame(type){
-                switch (type) {
-                    case 'Mega':
-                        amount-=4.5;
-                        this.createAmoutComponente()
-                        break;
-                    case 'Lotofácil':
-                        amount-=2.5;
-                        this.createAmoutComponente()
-                        break;
-                    case 'Quina':
-                        amount-=2;
-                        this.createAmoutComponente()
-                        break;
-                    default:
-                        alert('error')
-                        break;
-                }
-            },
+            removeAmoutGame: function removeAmoutGame(type) {
+                const remove = rules.filter(e=>{
+                    return type==e.type
+                })
+                console.log(remove)
+                amount-=remove[0].price
+                this.createAmoutComponente()
+            }
         }
     })()
     app.init()
